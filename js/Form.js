cc111dd140899602dcbf602a68f2360ac5176bd4
Sreeram2108 <86720588+Sreeram2108@users.noreply.github.com>
2021-08-26 19:02:38 +0800

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,45 @@
+class Form {
+  constructor() {
+    this.input = createInput("").attribute("placeholder", "Enter your name");
+    this.playButton = createButton("Play");
+    this.titleImg = createImg("./assets/title.png");
+    this.greeting = createElement("h2");
+  }
+
+  setElementsPosition() {
+    this.titleImg.position(120,50);
+    this.input.position(width / 2 - 110, height / 2 - 80);
+    this.playButton.position(width / 2 - 90, height / 2 - 20);
+    this.greeting.position(width / 2 - 500, height / 2 - 100);
+  }
+
+  setElementsStyle() {
+    this.titleImg.class("gameTitle");
+    this.input.class("customInput");
+    this.playButton.class("customButton");
+    this.greeting.class("greeting");
+  }
+
+  hide() {
+    this.greeting.hide();
+    this.playButton.hide();
+    this.input.hide();
+  }
+
+  //function(){}
+  //()=>{}   Arrow function
+
+handlenameless(){
+  this.playButton.mousePressed(()=>{
+    this.playButton.hide();
+    this.input.hide();
+    this.greeting.html('Hello '+this.input.value()+', wait for another player ')
+  })
+}   
+
+  display() {
+    this.setElementsPosition();
+    this.setElementsStyle();
+    this.handlenameless()
+  }
+}
